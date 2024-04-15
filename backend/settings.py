52681594b08d1fdd57daccb17da8090081d247c6
Richard Starrett <adiga.nikhil@gmail.com>
2024-04-15 16:07:25 -0400

    index page sections: education and base

--- backend/settings.py
+++ backend/settings.py
@@ -79,9 +79,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # c:\Users\richa\OneDrive\Documents\Code\venv\Scripts\python.exe
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio_db',
+        'USER': 'postgres',
+        'PASSWORD': 'N!kh!l',
+        'HOST': 'localhost', # or your database host
+        'PORT': '5432', # or your database port
     }
 }
 
